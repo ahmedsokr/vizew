2f867ae6403df51c2aa23fd52ce6dcd7012784c9
ahmedibra2003 <113306151+ahmedibra2003@users.noreply.github.com>
2023-05-09 21:16:24 +0300

    Add files via upload

--- Controller/DBController.php
+++ Controller/DBController.php
@@ -19,23 +19,46 @@ class DBController
     }
   }
 
-  public function closeConnection()
-  {
-    if ($this->connection) {
-      $this->connection->close();
-    } else {
-      echo " connection is not opened";
+public function closeConnection()
+{
+    if($this->connection){
+        $this->connection->close();
     }
-  }
-
-  public function select($qry)
-  {
-    $result = $this->connection->query($qry);
-    if (!$result) {
-      echo "Error : " . mysqli_errno($this->connection);
-      return false;
-    } else {
-      return $result->fetch_all(MYSQLI_ASSOC);
+    else {
+        echo " connection is not opened";
     }
-  }
 }
+
+    public function select($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_errno($this->connection);
+            return false;
+        }
+        else 
+        {
+           return $result->fetch_all(MYSQLI_ASSOC);
+        }
+      
+    }
+
+    public function insert($qry)
+    {
+        $result = $this->connection->query($qry);
+        if($result == false)
+        {
+            echo "Error in Query";
+            return false;
+        }
+        else
+        {
+            return $this->connection->insert_id;
+        }
+    }
+
+    
+}
+
+?>
